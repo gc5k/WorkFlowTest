ec820276124df9a1818fbe9adcce2a7db03365d4
gc5k <chenguobo@gmail.com>
2020-07-25 10:55:56 +0800

    alsHZ

--- WorkFlowTest.R
+++ WorkFlowTest.R
@@ -1,10 +1,7 @@
 library("workflowr")
 
-# Configure Git (only need to do once per computer)
-#wflow_git_config(user.name = "gc5k", user.email = "chenguobo@gmail.com")
-
 # Start a new workflowr project
-wflow_start("myproject")
+wflow_start("alsHZ", git = FALSE)
 
 # Build the site
 wflow_build()
@@ -13,9 +10,6 @@ wflow_build()
 #   1. Edit the R Markdown files in analysis/
 #   2. Edit the theme and layout in analysis/_site.yml
 #   3. Add new or copy existing R Markdown files to analysis/
-
-# Preview your changes
-wflow_build()
 
 # Publish the site, i.e. version the source code and HTML results
 wflow_publish("analysis/*", "Start my new project")
